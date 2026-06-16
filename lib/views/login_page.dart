55c4f347c8cc358f2c97547fd0c275f4b249a2e9
jiaxuan96 <p23014859@student.newinti.edu.my>
2026-06-16 22:40:37 +0800

    fix: make login page scrollable

--- lib/views/login_page.dart
+++ lib/views/login_page.dart
@@ -85,194 +85,199 @@ class _LoginPageState  extends State<LoginPage>{
             ],
           ),
         ),
-        child: SafeArea(
-          child: Column(
-            children: [
-              SizedBox(height: 100),
-              Text(
-                'VirtualQueue',
-                style: TextStyle(
-                  fontFamily: 'Kavoon',
-                  color: Color(0xFF006670),
-                  fontSize: 35,
-                ),
-              ),
-              SizedBox(height: 100),
-              Container(
-                padding: const EdgeInsets.all(20),
-                margin: const EdgeInsets.symmetric(horizontal: 20),
-                decoration: BoxDecoration(
-                  color: Color(0xFFF0F4F5),
-                  borderRadius: BorderRadius.circular(30),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withValues(alpha: 0.10),
-                      blurRadius: 20,
-                      offset: Offset(-2, -2),
+        child: SingleChildScrollView(
+          child: SafeArea(
+            child: Container(
+              margin: EdgeInsets.only(bottom: 50),
+              child: Column(
+                children: [
+                  SizedBox(height: 100),
+                  Text(
+                    'VirtualQueue',
+                    style: TextStyle(
+                      fontFamily: 'Kavoon',
+                      color: Color(0xFF006670),
+                      fontSize: 35,
                     ),
-                    BoxShadow(
-                      color: Colors.black.withValues(alpha: 0.10),
-                      blurRadius: 20,
-                      offset: Offset(2, 2),
-                    ),
-                  ],
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(height:30),
-                    Center(
-                      child: Text(
-                        'Login Now',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 28,
+                  ),
+                  SizedBox(height: 100),
+                  Container(
+                    padding: const EdgeInsets.all(20),
+                    margin: const EdgeInsets.symmetric(horizontal: 20),
+                    decoration: BoxDecoration(
+                      color: Color(0xFFF0F4F5),
+                      borderRadius: BorderRadius.circular(30),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withValues(alpha: 0.10),
+                          blurRadius: 20,
+                          offset: Offset(-2, -2),
                         ),
-                      ),
-                    ),
-                    SizedBox(height:35),
-                    Text(
-                      'Email',
-                      style: TextStyle(
-                        fontSize: 18,
-                      ),
-                    ),
-                    SizedBox(height:10),
-                    Container(
-                      decoration: inset.BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(40),
-                        boxShadow: [
-                          inset.BoxShadow(
-                            color: Colors.black.withValues(alpha: 0.25),
-                            blurRadius: 2,
-                            offset: Offset(0, 2),
-                            inset: true,
-                          ),
-                        ],
-                      ),
-                      child: TextField(
-                        controller: emailController,
-                        keyboardType: TextInputType.emailAddress,
-                        decoration: InputDecoration(
-                          hintText: 'example@gmail.com',
-                          hintStyle: TextStyle(
-                            color: Colors.grey,
-                          ),
-                          filled: false,
-                          fillColor: Color(0xFFFFFFFF),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(40),
-                            borderSide: BorderSide.none,
-                          ),
+                        BoxShadow(
+                          color: Colors.black.withValues(alpha: 0.10),
+                          blurRadius: 20,
+                          offset: Offset(2, 2),
                         ),
-                      ),
+                      ],
                     ),
-                    SizedBox(height:30),
-                    Text(
-                      'Password',
-                      style: TextStyle(
-                        fontSize: 18,
-                      ),
-                    ),
-                    SizedBox(height:10),
-                    Container(
-                      decoration: inset.BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(40),
-                        boxShadow: [
-                          inset.BoxShadow(
-                            color: Colors.black.withValues(alpha: 0.25),
-                            blurRadius: 2,
-                            offset: Offset(0, 2),
-                            inset: true,
-                          )
-                        ]
-                      ),
-                      child: TextField(
-                        controller: passwordController,
-                        obscureText: true,
-                        decoration: InputDecoration(
-                          hintText: 'password',
-                          hintStyle: TextStyle(
-                            color: Colors.grey,
-                          ),
-                          filled: false,
-                          fillColor: Color(0xFFFFFFFF),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(40),
-                            borderSide: BorderSide.none,
-                          ),
-                        ),
-                      ),
-                    ),
-                    SizedBox(height:30),
-                    Center(
-                      child: RichText(
-                        text: TextSpan(
-                          text: "No yet have an account? ",
-                          style: TextStyle(
-                            color: Colors.black,
-                          ),
-                          children: [
-                            TextSpan(
-                              text: 'Register here',
-                              style: TextStyle(
-                                color: Colors.blue,
-                                decoration: TextDecoration.underline,
-                              ),
-                              recognizer: TapGestureRecognizer()
-                              ..onTap = () {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (cotext) => RegistrationPage(),
-                                  ),
-                                );
-                              },
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(height:30),
+                        Center(
+                          child: Text(
+                            'Login Now',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 28,
                             ),
-                          ],
+                          ),
                         ),
-                      ),
-                    ),
-                    SizedBox(height:20),
-                    Center(
-                      child: InkWell(
-                        onTap: login,
-                        child: Container(
-                          width: 180,
-                          height: 65,
-                          decoration: BoxDecoration(
-                            color: Color(0xFF006670),
-                            borderRadius: BorderRadius.circular(30),
+                        SizedBox(height:35),
+                        Text(
+                          'Email',
+                          style: TextStyle(
+                            fontSize: 18,
+                          ),
+                        ),
+                        SizedBox(height:10),
+                        Container(
+                          decoration: inset.BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(40),
                             boxShadow: [
-                              BoxShadow(
+                              inset.BoxShadow(
                                 color: Colors.black.withValues(alpha: 0.25),
-                                blurRadius: 4,
-                                offset: Offset(2, 2),
+                                blurRadius: 2,
+                                offset: Offset(0, 2),
+                                inset: true,
                               ),
                             ],
                           ),
-                          padding: const EdgeInsets.all(10),
-                          child: Center(
-                            child: Text(
-                              'Login',
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
-                                color: Color(0xFFF6FAFB),
-                                fontSize: 30,
-                                fontFamily: 'JockeyOne',
+                          child: TextField(
+                            controller: emailController,
+                            keyboardType: TextInputType.emailAddress,
+                            decoration: InputDecoration(
+                              hintText: 'example@gmail.com',
+                              hintStyle: TextStyle(
+                                color: Colors.grey,
+                              ),
+                              filled: false,
+                              fillColor: Color(0xFFFFFFFF),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(40),
+                                borderSide: BorderSide.none,
                               ),
                             ),
                           ),
                         ),
-                      ),
-                    ),
-                    SizedBox(height:30),
-                  ],
-                )
+                        SizedBox(height:30),
+                        Text(
+                          'Password',
+                          style: TextStyle(
+                            fontSize: 18,
+                          ),
+                        ),
+                        SizedBox(height:10),
+                        Container(
+                          decoration: inset.BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(40),
+                            boxShadow: [
+                              inset.BoxShadow(
+                                color: Colors.black.withValues(alpha: 0.25),
+                                blurRadius: 2,
+                                offset: Offset(0, 2),
+                                inset: true,
+                              )
+                            ]
+                          ),
+                          child: TextField(
+                            controller: passwordController,
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              hintText: 'password',
+                              hintStyle: TextStyle(
+                                color: Colors.grey,
+                              ),
+                              filled: false,
+                              fillColor: Color(0xFFFFFFFF),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(40),
+                                borderSide: BorderSide.none,
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height:30),
+                        Center(
+                          child: RichText(
+                            text: TextSpan(
+                              text: "No yet have an account? ",
+                              style: TextStyle(
+                                color: Colors.black,
+                              ),
+                              children: [
+                                TextSpan(
+                                  text: 'Register here',
+                                  style: TextStyle(
+                                    color: Colors.blue,
+                                    decoration: TextDecoration.underline,
+                                  ),
+                                  recognizer: TapGestureRecognizer()
+                                  ..onTap = () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (cotext) => RegistrationPage(),
+                                      ),
+                                    );
+                                  },
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        SizedBox(height:20),
+                        Center(
+                          child: InkWell(
+                            onTap: login,
+                            child: Container(
+                              width: 180,
+                              height: 65,
+                              decoration: BoxDecoration(
+                                color: Color(0xFF006670),
+                                borderRadius: BorderRadius.circular(30),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.black.withValues(alpha: 0.25),
+                                    blurRadius: 4,
+                                    offset: Offset(2, 2),
+                                  ),
+                                ],
+                              ),
+                              padding: const EdgeInsets.all(10),
+                              child: Center(
+                                child: Text(
+                                  'Login',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    color: Color(0xFFF6FAFB),
+                                    fontSize: 30,
+                                    fontFamily: 'JockeyOne',
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height:30),
+                      ],
+                    )
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
         ),
       ),
